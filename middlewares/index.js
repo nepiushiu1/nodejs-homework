319d52d1a15a1de00c6f07452c4b5c08c2dcdf54
Сергей Власенко <98455988+nepiushiu1@users.noreply.github.com>
2023-02-26 13:18:18 +0200

    start

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,12 @@ const validation = require("./validation");
 const ctrlWrapper = require("./ctrlWrapper");
 const users = require("./users");
 const isValidId = require("./isValidId");
+const upload = require("./upload");
 
 module.exports = {
   validation,
   ctrlWrapper,
   users,
   isValidId,
+  upload,
 };
