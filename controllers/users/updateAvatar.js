10c0cf6fbfb50b81434313c43b486942d406a073
Сергей Власенко <98455988+nepiushiu1@users.noreply.github.com>
2023-02-28 16:09:03 +0200

    finish

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -2,17 +2,16 @@ const User = require("../../models/user");
 const path = require("path");
 const fs = require("fs/promises");
 const Jimp = require("jimp");
+const { v4 } = require("uuid");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
+  const { path: tempUpload, originalname } = req.file;
+  const { _id: id } = req.user;
+
+  const imageName = `${id}_${originalname}_${v4()}`;
   try {
-    const { path: tempUpload, originalname } = req.file;
-    const { _id: id } = req.user;
-    const [extenstion] = originalname.split(".").reverse();
-
-    const imageName = `${id}_${extenstion}`;
-
     const resultUpload = path.join(avatarsDir, imageName);
     Jimp.read(tempUpload, (error, image) => {
       if (error) throw error;
